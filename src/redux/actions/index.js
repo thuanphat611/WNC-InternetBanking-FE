5e35cdc74d1d041bc70699f8f4fc5626a259f2bd
thuanphat611 <phat06112003@gmail.com>
2024-12-29 15:27:52 +0700

    New action to get user's information

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,12 @@
-import { AuthorizationActionTypes } from "./actionTypes";
+import {
+  AuthorizationActionTypes,
+  UserInformationActionTypes,
+} from "./actionTypes";
+
 const PORT_SERVER = process.env.PORT_SERVER || 5000;
 const HOST_SERVER = process.env.HOST_SERVER || "localhost";
 const URL_API = `http://${HOST_SERVER}:${PORT_SERVER}`;
+
 export const AuthorizationActionCreators = {
   setUserAccessToken: (accessToken) => ({
     type: AuthorizationActionTypes.AUTH_SET_ACCESS_TOKEN,
@@ -11,5 +16,12 @@ export const AuthorizationActionCreators = {
     // value: "true" or "false"
     type: AuthorizationActionTypes.AUTH_SET_IS_AUTHENTICATED,
     payload: value,
+  }),
+};
+
+export const UserInformationActionCreators = {
+  getAllInformation: (objectUser) => ({
+    type: UserInformationActionTypes.USER_GET_ALL_INFORMATION,
+    payload: objectUser,
   }),
 };
