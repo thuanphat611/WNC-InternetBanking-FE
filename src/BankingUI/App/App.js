08e49612b88e838f163cc0f6551f98b223d6a2c2
thuanphat611 <phat06112003@gmail.com>
2024-12-30 14:26:31 +0700

    Authorization for admin and customer in front-end

--- src/BankingUI/App/App.js
+++ src/BankingUI/App/App.js
@@ -1,113 +1,155 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Redirect,
-	Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
 } from "react-router-dom";
 
-import { public_routes, private_routes } from "../../routes/appRoutes";
+import {
+  public_routes,
+  customer_routes,
+  admin_routes,
+} from "../../routes/appRoutes";
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 const App = (props) => {
-	const {
-		reducerAuthorization,
-		setUserAccessToken,
-		setIsAuthenticated,
-		getAllInformation,
-	} = props;
-	const {
-		isAuthenticated,
-		authentication,
-		userInformation,
-	} = reducerAuthorization;
-	const localAccessToken = localStorage.getItem("token");
-	const mountedRef = useRef(true);
+  const {
+    reducerAuthorization,
+    setUserAccessToken,
+    setIsAuthenticated,
+    getAllInformation,
+    setRole,
+  } = props;
+  const { isAuthenticated, authentication } = reducerAuthorization;
+  const { role } = authentication;
+  const localAccessToken = localStorage.getItem("token");
+  const mountedRef = useRef(true);
 
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-		console.log(`hello world ${localAccessToken}`);
-		if (localAccessToken) {
-			if (!authentication.accessToken) setUserAccessToken(localAccessToken);
-		} else setUserAccessToken(null);
+    console.log(`hello world ${localAccessToken}`);
+    if (localAccessToken) {
+      if (!authentication.accessToken) setUserAccessToken(localAccessToken);
+    } else setUserAccessToken(null);
 
-		return () => {
-			mountedRef.current = false;
-		};
-	}, []);
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
 
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-		if (authentication.accessToken) {
-			axios
-				.get("http://localhost:5000/api/users/me", {
-					headers: { Authorization: `Bearer ${authentication.accessToken}` },
-				})
-				.then((result) => {
-					if (result.status === 200) {
-						console.log(result);
-						setIsAuthenticated(true);
-						getAllInformation(result.data);
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-					setIsAuthenticated(false);
-					localStorage.removeItem("token");
-				});
-		}
-		if (!authentication.accessToken && !localAccessToken) {
-			setIsAuthenticated(false);
-			localStorage.removeItem("token");
-		}
-	}, [authentication.accessToken]);
+    if (authentication.accessToken) {
+      setRole(jwtDecode(authentication.accessToken).role);
+      axios
+        .get("http://localhost:5000/api/users/me", {
+          headers: { Authorization: `Bearer ${authentication.accessToken}` },
+        })
+        .then((result) => {
+          if (result.status === 200) {
+            setIsAuthenticated(true);
+            getAllInformation(result.data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsAuthenticated(false);
+          localStorage.removeItem("token");
+        });
+    }
+    if (!authentication.accessToken && !localAccessToken) {
+      setIsAuthenticated(false);
+      localStorage.removeItem("token");
+    }
+  }, [authentication.accessToken]);
 
-	return (
-		<div className="app">
-			<Router>
-				<Switch>
-					{/* https://stackoverflow.com/questions/43520498/react-router-private-routes-redirect-not-working => Newm */}
-					{public_routes.map((item, index) => {
-						return (
-							<Route
-								key={index}
-								path={item.path}
-								exact={item.exact ? item.exact : null}
-								component={(props) => (
-									<item.component
-										{...props}
-										{...item.props}
-										isAuthenticated={isAuthenticated}
-									/>
-								)}
-							/>
-						);
-					})}
-					{private_routes.map((item, index) => {
-						return (
-							<item.routetype
-								key={index}
-								path={item.path}
-								exact={item.exact ? item.exact : null}
-								isAuthenticated={isAuthenticated}
-								comp={(props) => {
-									return item.layout ? (
-										<item.layout>
-											<item.component {...props} {...item.props} />
-										</item.layout>
-									) : (
-										<item.component {...props} {...item.props} />
-									);
-								}}
-							/>
-						);
-					})}
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          {/* https://stackoverflow.com/questions/43520498/react-router-private-routes-redirect-not-working => Newm */}
+          {/* --- LOGIN/PUBLIC COMPONENTS RENDER --- */}
+          {public_routes.map((item, index) => {
+            return (
+              <Route
+                key={index}
+                path={item.path}
+                exact={item.exact ? item.exact : null}
+                component={(props) => (
+                  <item.component
+                    {...props}
+                    {...item.props}
+                    isAuthenticated={isAuthenticated}
+                  />
+                )}
+              />
+            );
+          })}
+          {!isAuthenticated &&
+            public_routes.map((item, index) => {
+              return (
+                <Redirect
+                  to={{
+                    pathname: "/login",
+                    // state: { from: props.location },
+                  }}
+                />
+              );
+            })}
+
+          {/* ---CUSTOMER RENDER --- */}
+          {isAuthenticated &&
+            role === "customer" &&
+            customer_routes.map((item, index) => {
+              return (
+                <item.routetype
+                  key={index}
+                  path={item.path}
+                  exact={item.exact ? item.exact : null}
+                  isAuthenticated={isAuthenticated}
+                  comp={(props) => {
+                    return item.layout ? (
+                      <item.layout>
+                        <item.component {...props} {...item.props} />
+                      </item.layout>
+                    ) : (
+                      <item.component {...props} {...item.props} />
+                    );
+                  }}
+                />
+              );
+            })}
+
+          {/* ---ADMIN RENDER --- */}
+          {isAuthenticated &&
+            role === "admin" &&
+            admin_routes.map((item, index) => {
+              return (
+                <item.routetype
+                  key={index}
+                  path={item.path}
+                  exact={item.exact ? item.exact : null}
+                  isAuthenticated={isAuthenticated}
+                  comp={(props) => {
+                    return item.layout ? (
+                      <item.layout>
+                        <item.component {...props} {...item.props} />
+                      </item.layout>
+                    ) : (
+                      <item.component {...props} {...item.props} />
+                    );
+                  }}
+                />
+              );
+            })}
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
