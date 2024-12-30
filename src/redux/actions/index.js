c81eff93cb5d249786188e010888ed4fd8314d58
thuanphat611 <phat06112003@gmail.com>
2024-12-30 14:23:55 +0700

    Add role properties for authorization + update role action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,27 +1,47 @@
 import {
-	AuthorizationActionTypes,
-	UserInformationActionTypes,
+  AuthorizationActionTypes,
+  UserInformationActionTypes,
 } from "./actionTypes";
+import axios from "axios";
 
 const PORT_SERVER = process.env.PORT_SERVER || 5000;
 const HOST_SERVER = process.env.HOST_SERVER || "localhost";
 const URL_API = `http://${HOST_SERVER}:${PORT_SERVER}`;
 
 export const AuthorizationActionCreators = {
-	setUserAccessToken: (accessToken) => ({
-		type: AuthorizationActionTypes.AUTH_SET_ACCESS_TOKEN,
-		payload: accessToken,
-	}),
-	setIsAuthenticated: (value) => ({
-		// value: "true" or "false"
-		type: AuthorizationActionTypes.AUTH_SET_IS_AUTHENTICATED,
-		payload: value,
-	}),
+  setUserAccessToken: (accessToken) => ({
+    type: AuthorizationActionTypes.AUTH_SET_ACCESS_TOKEN,
+    payload: accessToken,
+  }),
+  setIsAuthenticated: (value) => ({
+    // value: "true" or "false"
+    type: AuthorizationActionTypes.AUTH_SET_IS_AUTHENTICATED,
+    payload: value,
+  }),
+  setRole: (value) => ({
+    type: AuthorizationActionTypes.AUTH_SET_ROLE,
+    payload: value,
+  }),
 };
 
 export const UserInformationActionCreators = {
-	getAllInformation: (objectUser) => ({
-		type: UserInformationActionTypes.USER_GET_ALL_INFORMATION,
-		payload: objectUser,
-	}),
+  getAllInformation: (objectUser) => ({
+    type: UserInformationActionTypes.USER_GET_ALL_INFORMATION,
+    payload: objectUser,
+  }),
+  updateUserInfo: (entity, token) => ({
+    type: UserInformationActionTypes.USER_UPDATE_INFORMATION,
+    payload: new Promise((resolve, reject) =>
+      axios
+        .patch(`${URL_API}/api/users/me`, entity, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          reject(error);
+        })
+    ),
+  }),
 };
--- src/redux/reducers/userInformation.reducer.js
+++ src/redux/reducers/userInformation.reducer.js
@@ -1,38 +1,75 @@
 import { UserInformationActionTypes } from "../actions/actionTypes";
 
 const initialState = {
-	id: -1,
-	accountNumber: "",
-	username: "",
-	name: "",
-	email: "",
-	phone: "",
-	receivers: [],
-	status: true,
-	role: "",
-	balance: -1,
+  data: {
+    id: -1,
+    accountNumber: "",
+    username: "",
+    name: "",
+    email: "",
+    phone: "",
+    receivers: [],
+    status: true,
+    role: "",
+    balance: -1,
+  },
+  isLoading: false,
+  message: null,
+  error: null,
 };
 
 const reducerUserInformation = (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case UserInformationActionTypes.USER_GET_ALL_INFORMATION: {
-			return {
-				...state,
-				id: payload._id,
-				accountNumber: payload.accountNumber,
-				username: payload.username,
-				name: payload.name,
-				email: payload.email,
-				phone: payload.phone,
-				status: payload.status,
-				role: payload.role,
-				balance: payload.balance,
-			};
-		}
-		default:
-			return state;
-	}
+  const { type, payload } = action;
+  switch (type) {
+    case UserInformationActionTypes.USER_GET_ALL_INFORMATION: {
+      return {
+        ...state,
+        data: {
+          id: payload._id,
+          accountNumber: payload.accountNumber,
+          username: payload.username,
+          name: payload.name,
+          email: payload.email,
+          phone: payload.phone,
+          status: payload.status,
+          role: payload.role,
+          balance: payload.balance,
+        },
+      };
+    }
+    case UserInformationActionTypes.USER_UPDATE_INFORMATION_PENDING: {
+      return {
+        ...state,
+        isLoading: true,
+        message: null,
+        error: null,
+      };
+    }
+    case UserInformationActionTypes.USER_UPDATE_INFORMATION_FULFILLED: {
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          name: payload.name,
+          email: payload.email,
+          phone: payload.phone,
+        },
+        isLoading: false,
+        message: null,
+        error: null,
+      };
+    }
+    case UserInformationActionTypes.USER_UPDATE_INFORMATION_PENDING: {
+      return {
+        ...state,
+        isLoading: false,
+        message: "Error when updating information!",
+        error: payload,
+      };
+    }
+    default:
+      return state;
+  }
 };
 
 export default reducerUserInformation;
