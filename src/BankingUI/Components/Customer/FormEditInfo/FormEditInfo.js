ad317073cfac5cdff3dab29d4f4a20e131a1e5fa
thuanphat611 <phat06112003@gmail.com>
2024-12-31 14:39:05 +0700

    Change password, show error/success box

--- src/BankingUI/Components/Customer/FormEditInfo/FormEditInfo.js
+++ src/BankingUI/Components/Customer/FormEditInfo/FormEditInfo.js
@@ -5,91 +5,103 @@ import axios from "axios";
 
 import BasicInfoForm from "./BasicInfoForm/BasicInfoForm";
 import PasswordForm from "./PasswordForm/PasswordForm";
+import AlertBox from "../../Others/AlertBox/AlertBox";
 import "./FormEditInfo.css";
 
 const FormEditInfo = (props) => {
-	const {
-		reducerAuthorization,
-		reducerUserInformation,
-		updateUserInfo,
-	} = props;
-	const { accessToken } = reducerAuthorization.authentication;
-	const {
-		accountNumber,
-		username,
-		name,
-		phone,
-		email,
-	} = reducerUserInformation.data;
-	const [basicInfoForm, setBasicInfoForm] = useState({
-		accountNumber: accountNumber,
-		username: username,
-		name: name,
-		phone: phone,
-		email: email,
-	});
-	const [passwordForm, setPasswordForm] = useState({
-		accountNumber: accountNumber,
-		username: username,
-		currentPassword: "",
-		newPassword: "",
-		retypeNewPassword: "",
-	});
+  const { reducerAuthorization, reducerUserInformation, updateUserInfo } =
+    props;
+  const { accessToken } = reducerAuthorization.authentication;
+  const { accountNumber, username, name, phone, email } =
+    reducerUserInformation.data;
+  const [basicInfoForm, setBasicInfoForm] = useState({
+    accountNumber: accountNumber,
+    username: username,
+    name: name,
+    phone: phone,
+    email: email,
+    currentPassword: "",
+    newPassword: "",
+    retypeNewPassword: "",
+    message: "",
+    error: "",
+  });
 
-	const [showBasicForm, setShowBasicForm] = useState(true);
+  const [showBasicForm, setShowBasicForm] = useState(true);
 
-	return (
-		<Container fluid>
-			<Row>
-				<Col md={{ span: 6, offset: 3 }} lg={6}>
-					<Card className="text-center" className="mt-3">
-						<Card.Header className="text-center toolbar">
-							<span>EDIT INFORMATION</span>
+  const renderAlert = () => {
+    if (basicInfoForm.message)
+      return (
+        <AlertBox
+          alertTypes={basicInfoForm.error}
+          alertMessage={basicInfoForm.message}
+        />
+      );
+  };
 
-							{showBasicForm === true ? (
-								<Link
-									onClick={() => {
-										setShowBasicForm(false);
-									}}
-								>
-									Change Password
-								</Link>
-							) : (
-								<Link
-									onClick={() => {
-										setShowBasicForm(true);
-									}}
-								>
-									Basic
-								</Link>
-							)}
-						</Card.Header>
-						<Card.Body>
-							{showBasicForm === true ? (
-								<BasicInfoForm
-									basicInfoForm={basicInfoForm}
-									setBasicInfoForm={setBasicInfoForm}
-									updateUserInfo={updateUserInfo}
-									accessToken={accessToken}
-									setShowBasicForm={setShowBasicForm}
-								/>
-							) : (
-								<PasswordForm
-									passwordForm={passwordForm}
-									setPasswordForm={setPasswordForm}
-									accessToken={accessToken}
-									setShowBasicForm={setShowBasicForm}
-								/>
-							)}
-						</Card.Body>
-						<Card.Footer className="text-muted text-center">
-							HCMUS - PTUDWNC - 2019
-						</Card.Footer>
-					</Card>
-				</Col>
-			</Row>
-		</Container>
-	);
+  const setFormError = (error, message) => {
+    let alertTypes = error === null ? "success" : "danger";
+
+    basicInfoForm["error"] = alertTypes;
+    setBasicInfoForm({ ...basicInfoForm });
+
+    basicInfoForm["message"] = message;
+    setBasicInfoForm({ ...basicInfoForm });
+  };
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }} lg={6}>
+          <Card className="text-center" className="mt-3">
+            <Card.Header className="text-center toolbar">
+              <span>EDIT INFORMATION</span>
+
+              {showBasicForm === true ? (
+                <Link
+                  onClick={() => {
+                    setShowBasicForm(false);
+                  }}
+                >
+                  Change Password
+                </Link>
+              ) : (
+                <Link
+                  onClick={() => {
+                    setShowBasicForm(true);
+                  }}
+                >
+                  Basic
+                </Link>
+              )}
+            </Card.Header>
+            <Card.Body>
+              {renderAlert()}
+              {showBasicForm === true ? (
+                <BasicInfoForm
+                  basicInfoForm={basicInfoForm}
+                  setBasicInfoForm={setBasicInfoForm}
+                  updateUserInfo={updateUserInfo}
+                  accessToken={accessToken}
+                  setShowBasicForm={setShowBasicForm}
+                />
+              ) : (
+                <PasswordForm
+                  passwordForm={basicInfoForm}
+                  setPasswordForm={setBasicInfoForm}
+                  accessToken={accessToken}
+                  setFormError={setFormError}
+                />
+              )}
+            </Card.Body>
+            <Card.Footer className="text-muted text-center">
+              HCMUS - PTUDWNC - 2019
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default FormEditInfo;
