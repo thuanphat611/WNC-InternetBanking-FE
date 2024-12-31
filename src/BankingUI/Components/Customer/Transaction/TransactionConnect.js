9a74de76c985b1374fa78d77c7213a4d6169ef3f
thuanphat611 <phat06112003@gmail.com>
2024-12-31 14:40:53 +0700

    Fix some warnings

--- src/BankingUI/Components/Customer/Transaction/TransactionConnect.js
+++ src/BankingUI/Components/Customer/Transaction/TransactionConnect.js
@@ -1,26 +1,23 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import {
-	AuthorizationActionCreators,
-	UserInformationActionCreators,
+  // AuthorizationActionCreators,
+  UserInformationActionCreators,
 } from "../../../../redux/actions/index";
 import Transaction from "./Transaction";
 
 const mapStateToProps = (state) => {
-	return {
-		reducerAuthorization: state.reducerAuthorization,
-		reducerUserInformation: state.reducerUserInformation,
-	};
+  return {
+    reducerAuthorization: state.reducerAuthorization,
+    reducerUserInformation: state.reducerUserInformation,
+  };
 };
 
 const mapDispatchToProps = {
-	setUserAccessToken: AuthorizationActionCreators.setUserAccessToken,
-	setIsAuthenticated: AuthorizationActionCreators.setIsAuthenticated,
-	setRole: AuthorizationActionCreators.setRole,
-	getAllInformation: UserInformationActionCreators.getAllInformation,
-	updateUserInfo: UserInformationActionCreators.updateUserInfo,
+  getAllReceivers: UserInformationActionCreators.getAllReceivers,
+  getReceiverList: UserInformationActionCreators.getReceiverList,
 };
 
 export default withRouter(
-	connect(mapStateToProps, mapDispatchToProps)(Transaction)
+  connect(mapStateToProps, mapDispatchToProps)(Transaction)
 );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./BankingUI/App/App";
 import AppConnect from "./BankingUI/App/AppConnect";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -11,12 +10,12 @@ import * as serviceWorker from "./serviceWorker";
 require("dotenv").config();
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<AppConnect />
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppConnect />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
