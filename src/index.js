e6debffe779173418314735391c371234641563f
thuanphat611 <phat06112003@gmail.com>
2025-01-03 14:04:31 +0700

    Add antd css to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
 import AppConnect from "./BankingUI/App/AppConnect";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -10,12 +11,12 @@ import * as serviceWorker from "./serviceWorker";
 require("dotenv").config();
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<AppConnect />
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppConnect />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
