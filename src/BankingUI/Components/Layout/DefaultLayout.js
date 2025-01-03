5ee8b318c5e182e067deddfb1ba01a0c4479dab7
thuanphat611 <phat06112003@gmail.com>
2025-01-03 13:53:56 +0700

    Add jumbotron for customer's dashboard

--- src/BankingUI/Components/Layout/DefaultLayout.js
+++ src/BankingUI/Components/Layout/DefaultLayout.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
 
 import NavbarConnect from "./Navbar/NavbarConnect";
+import JumbotronConnect from "./JumbotronComponent/JumbotronComponentConnect";
 
 const DefaultLayout = ({ children }) => {
-	return (
-		<Fragment>
-			<NavbarConnect />
-			{children}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <NavbarConnect />
+      <JumbotronConnect />
+      {children}
+    </Fragment>
+  );
 };
 
 export default DefaultLayout;
