24e89fd413bce1977571623fd0efbf96ec9715ff
thuanphat611 <phat06112003@gmail.com>
2025-01-03 14:05:49 +0700

    Add dropdown component and css it

--- src/BankingUI/Components/Customer/TransactionManagement/TransactionManagement.js
+++ src/BankingUI/Components/Customer/TransactionManagement/TransactionManagement.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Container, Col, Card, Row, Badge, Alert } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Card,
+  Row,
+  DropdownButton,
+  Dropdown,
+  ButtonGroup,
+} from "react-bootstrap";
 import axios from "axios";
 
 import AlertBox from "../../Others/AlertBox/AlertBox";
@@ -9,97 +17,82 @@ import TransactionList from "./TransactionList/TransactionList";
 import moneyFormatter from "../../HelperFunctions/moneyFormatter";
 
 const TransactionManagement = (props) => {
-	const {
-		reducerAuthorization,
-		reducerUserInformation,
-		reducerUserTransactions,
-	} = props;
-	const currentUser = reducerUserInformation.data;
-	// const transactionsData = reducerUserTransactions.data;
-	// const transactionsData = [];
-	const [transactionsData, setTransactionsData] = useState([]);
-	const [formVariables, setFormVariables] = useState({
-		accountNumber: "",
-		bankId: 0,
-		name: "",
-		savedName: "",
-		amount: 50000,
-		content: "",
-		otp: "",
-		isDebt: 0,
-		isReceiverPaid: false,
-		transactionId: "",
-		createdAt: "",
-		error: null,
-		message: "",
-	});
-	const mountedRef = useRef(true);
+  const {
+    reducerAuthorization,
+    reducerUserInformation,
+    reducerUserTransactions,
+  } = props;
+  const currentUser = reducerUserInformation.data;
+  // const transactionsData = reducerUserTransactions.data;
+  // const transactionsData = [];
+  const [transactionsData, setTransactionsData] = useState([]);
+  const mountedRef = useRef(true);
 
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-		let isGettingAList = true;
-		getList(isGettingAList);
+    let isGettingAList = true;
+    getList(isGettingAList);
 
-		return () => {
-			mountedRef.current = false;
-			isGettingAList = false;
-		};
-	}, []);
+    return () => {
+      mountedRef.current = false;
+      isGettingAList = false;
+    };
+  }, []);
 
-	const getList = async (isGettingAList) => {
-		await axios
-			.get(`/api/transaction/history`)
-			.then((result) => result.data.data)
-			.then((result) => {
-				if (isGettingAList) {
-					result.reverse();
-					console.log(result);
-					setTransactionsData(result);
-					isGettingAList = false;
-				}
-			})
-			.catch((error) => {
-				console.log(error.response);
-			});
-	};
+  const getList = async (isGettingAList) => {
+    await axios
+      .get(`/api/transaction/history`)
+      .then((result) => result.data.data)
+      .then((result) => {
+        if (isGettingAList) {
+          result.reverse();
+          console.log(result);
+          setTransactionsData(result);
+          isGettingAList = false;
+        }
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
-	return (
-		<Container fluid>
-			<Row>
-				<Col md={{ span: 5, offset: 3 }} lg={6}>
-					<Card className="mt-3">
-						<Card.Header className="toolBar">
-							QUẢN LÝ GIAO DỊCH TÀI KHOẢN
-						</Card.Header>
-						<Card.Body>
-							<div className="form-group row">
-								<label
-									for="example-date-input"
-									className="col-4 col-form-label"
-								>
-									Date
-								</label>
-								<div className="col-8">
-									<input
-										className="form-control"
-										type="date"
-										value={Date()}
-										onChange={(e) => console.log(e.target.value)}
-										id="example-date-input"
-									/>
-								</div>
-							</div>
-							<TransactionList
-								transactionsData={transactionsData}
-								currentUser={currentUser}
-							/>
-						</Card.Body>
-					</Card>
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={{ span: 5, offset: 3 }} lg={6}>
+          <Card className="mt-3">
+            <Card.Header className="toolBar">
+              QUẢN LÝ GIAO DỊCH TÀI KHOẢN
+            </Card.Header>
+            <Card.Body>
+              <div className="form-group row">
+                <label
+                  for="example-date-input"
+                  className="col-4 col-form-label"
+                >
+                  Sort
+                </label>
+                <DropdownButton
+                  as={ButtonGroup}
+                  id="dropdown-variants-light"
+                  variant="light"
+                  title="Date"
+                >
+                  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                </DropdownButton>
+              </div>
+              <TransactionList
+                transactionsData={transactionsData}
+                currentUser={currentUser}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default TransactionManagement;
