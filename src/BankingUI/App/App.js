fd339d4698a089d35741d96764b50633c20875a3
thuanphat611 <phat06112003@gmail.com>
2025-01-05 15:28:11 +0700

    Not refresh when get 401 on LONGPOLLING req

--- src/BankingUI/App/App.js
+++ src/BankingUI/App/App.js
@@ -1,338 +1,345 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Redirect,
-	Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
 } from "react-router-dom";
 import openNotification from "../Components/HelperFunctions/openNotification";
 
 import {
-	public_routes,
-	customer_routes,
-	admin_routes,
-	employee_routes,
+  public_routes,
+  customer_routes,
+  admin_routes,
+  employee_routes,
 } from "../../routes/appRoutes";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
+const axiosInstance = axios.create();
+
 // import socketIO from "../../service/socket";
 
 const App = (props) => {
-	const {
-		reducerAuthorization,
-		reducerUserNotification,
-		setUserAccessToken,
-		setIsAuthenticated,
-		getAllInformation,
-		setRole,
-		getAllReceivers,
-		getAllTransactions,
-		setUserRefreshToken,
-		setAllNotification,
-	} = props;
-	const { isAuthenticated, authentication } = reducerAuthorization;
-	const { role } = authentication;
-	const localAccessToken = localStorage.getItem("token");
-	const localRefreshToken = localStorage.getItem("refreshToken");
-	const mountedRef = useRef(true);
-	// const [notificationsData, setNotificationsData] = useState([]);
-	const notificationsData = reducerUserNotification.data;
+  const {
+    reducerAuthorization,
+    reducerUserNotification,
+    setUserAccessToken,
+    setIsAuthenticated,
+    getAllInformation,
+    setRole,
+    getAllReceivers,
+    getAllTransactions,
+    setUserRefreshToken,
+    setAllNotification,
+  } = props;
+  const { isAuthenticated, authentication } = reducerAuthorization;
+  const { role } = authentication;
+  const localAccessToken = localStorage.getItem("token");
+  const localRefreshToken = localStorage.getItem("refreshToken");
+  const mountedRef = useRef(true);
+  // const [notificationsData, setNotificationsData] = useState([]);
+  const notificationsData = reducerUserNotification.data;
 
-	// --- CONFIG AXIOS ---
-	axios.defaults.baseURL = "http://localhost:5000";
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `Bearer ${authentication.accessToken}`;
-	// axios.defaults.timeout = 15000;
+  // --- CONFIG AXIOS ---
+  axios.defaults.baseURL = "http://localhost:5000";
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${authentication.accessToken}`;
+  axiosInstance.defaults.baseURL = "http://localhost:5000";
+  axiosInstance.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${authentication.accessToken}`;
 
-	// https://medium.com/@monkov/react-using-axios-interceptor-for-token-refreshing-1477a4d5fc26
-	axios.interceptors.response.use(
-		(response) => {
-			return response;
-		},
-		(err) => {
-			console.log(err.response);
-			if (
-				err.response.data.message === "Invalid username or password!" ||
-				err.response.data.from === "LOGIN"
-			)
-				return err.response.data;
+  // axios.defaults.timeout = 15000;
 
-			return new Promise((resolve, reject) => {
-				const originalReq = err.config;
+  // https://medium.com/@monkov/react-using-axios-interceptor-for-token-refreshing-1477a4d5fc26
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (err) => {
+      console.log(err.response);
+      if (
+        err.response.data.message === "Invalid username or password!" ||
+        err.response.data.from === "LOGIN"
+      )
+        return err.response.data;
 
-				if (
-					err.response.status === 401 &&
-					err.config &&
-					!err.config.__isRetryRequest &&
-					err.response.data.message !== "Invalid username or password!"
-				) {
-					originalReq._retry = true;
+      return new Promise((resolve, reject) => {
+        const originalReq = err.config;
 
-					let res = fetch("http://localhost:5000/api/auth/refresh", {
-						method: "POST",
-						mode: "cors",
-						cache: "no-cache",
-						credentials: "same-origin",
-						headers: {
-							"Content-Type": "application/json",
-							Device: "device",
-							Token: localStorage.getItem("token"),
-						},
-						redirect: "follow",
-						referrer: "no-referrer",
-						body: JSON.stringify({
-							accessToken: localStorage.getItem("token"),
-							refreshToken: localStorage.getItem("refreshToken"),
-						}),
-					})
-						.then((res) => res.json())
-						.then((res) => {
-							console.log(res);
-							originalReq.headers[
-								"Authorization"
-							] = `Bearer ${res.accessToken}`;
-							originalReq.headers["Device"] = "device";
-							localStorage.setItem("token", res.accessToken);
-							setUserAccessToken(res.accessToken);
+        if (
+          err.response.status === 401 &&
+          err.config &&
+          !err.config.__isRetryRequest &&
+          err.response.data.message !== "Invalid username or password!"
+        ) {
+          originalReq._retry = true;
 
-							return axios(originalReq);
-						});
+          let res = fetch("http://localhost:5000/api/auth/refresh", {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+              "Content-Type": "application/json",
+              Device: "device",
+              Token: localStorage.getItem("token"),
+            },
+            redirect: "follow",
+            referrer: "no-referrer",
+            body: JSON.stringify({
+              accessToken: localStorage.getItem("token"),
+              refreshToken: localStorage.getItem("refreshToken"),
+            }),
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res);
+              originalReq.headers[
+                "Authorization"
+              ] = `Bearer ${res.accessToken}`;
+              originalReq.headers["Device"] = "device";
+              localStorage.setItem("token", res.accessToken);
+              setUserAccessToken(res.accessToken);
 
-					resolve(res);
-				}
+              return axios(originalReq);
+            });
 
-				return Promise.reject(err);
-			});
-		}
-	);
+          resolve(res);
+        }
 
-	// --- GETTING NOTIFICATION && LONG POLLING WITH SERVER
-	let isGettingAList = true;
-	let isQueryingNotification = true;
+        return Promise.reject(err);
+      });
+    }
+  );
 
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  // --- GETTING NOTIFICATION && LONG POLLING WITH SERVER
+  let isGettingAList = true;
+  let isQueryingNotification = true;
 
-		return () => {
-			mountedRef.current = false;
-			isGettingAList = false;
-			isQueryingNotification = false;
-			console.log("isLoading: ", isQueryingNotification);
-		};
-	}, []);
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-	let ts = 0;
-	let flag = 0;
+    return () => {
+      mountedRef.current = false;
+      isGettingAList = false;
+      isQueryingNotification = false;
+      console.log("isLoading: ", isQueryingNotification);
+    };
+  }, []);
 
-	const getNotificationHistory = async () => {
-		if (isQueryingNotification === false) return;
-		// const fn = () => {
-		await axios
-			.get(`/api/notification/history?ts=${ts}`)
-			.then((result) => {
-				console.log("isloading at fn: ", isQueryingNotification);
-				if (result.status === 200) {
-					if (result.data.return_ts !== ts) {
-						ts = result.data.return_ts;
-						console.log("ts: ", ts);
-						return result.data.data;
-					}
-					// return result.data.data;
-				} else {
-					if (isQueryingNotification) getNotificationHistory();
-				}
-			})
-			.then((result) => {
-				// if (!isLoading) return;
-				let newArray = reducerUserNotification.data.slice();
-				result.forEach((element) => {
-					newArray.push(element);
-					if (flag !== 0) {
-						openNotification(
-							element.notificationTitle,
-							element.notificationContent
-						);
-					}
-				});
-				setAllNotification(newArray);
-				// setNotificationsData((notificationsData) => [
-				// 	element,
-				// 	...notificationsData,
-				// ]);
-				// setAllNotification(notificationsData);
-				console.log(newArray, ts);
-				// isGettingAList = false;
-				flag++;
-				console.log("flag: ", flag);
-				if (isQueryingNotification) {
-					getNotificationHistory();
-				}
-			})
-			.catch((error) => {});
-		// };
-	};
+  let ts = 0;
+  let flag = 0;
 
-	// Check localStorage variables
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  const getNotificationHistory = async () => {
+    if (isQueryingNotification === false) return;
+    // const fn = () => {
+    await axiosInstance
+      .get(`/api/notification/history?ts=${ts}`)
+      .then((result) => {
+        console.log("isloading at fn: ", isQueryingNotification);
+        if (result.status === 200) {
+          if (result.data.return_ts !== ts) {
+            ts = result.data.return_ts;
+            console.log("ts: ", ts);
+            return result.data.data;
+          }
+          // return result.data.data;
+        } else {
+          if (isQueryingNotification) getNotificationHistory();
+        }
+      })
+      .then((result) => {
+        // if (!isLoading) return;
+        let newArray = reducerUserNotification.data.slice();
+        result.forEach((element) => {
+          newArray.push(element);
+          if (flag !== 0) {
+            openNotification(
+              element.notificationTitle,
+              element.notificationContent
+            );
+          }
+        });
+        setAllNotification(newArray);
+        // setNotificationsData((notificationsData) => [
+        // 	element,
+        // 	...notificationsData,
+        // ]);
+        // setAllNotification(notificationsData);
+        console.log(newArray, ts);
+        // isGettingAList = false;
+        flag++;
+        console.log("flag: ", flag);
+        if (isQueryingNotification) {
+          getNotificationHistory();
+        }
+      })
+      .catch((error) => {});
+    // };
+  };
 
-		console.log(localAccessToken);
-		if (localAccessToken && localRefreshToken) {
-			if (!authentication.accessToken || !authentication.refreshToken) {
-				setUserAccessToken(localAccessToken);
-				setUserRefreshToken(localRefreshToken);
-			}
-		} else {
-			setUserAccessToken(null);
-			setUserRefreshToken(null);
-			localStorage.removeItem("token");
-			localStorage.removeItem("refreshToken");
-		}
-	}, [localAccessToken, localRefreshToken]);
+  // Check localStorage variables
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-	// Check if accessToken is valid
-	useEffect(() => {
-		if (!mountedRef.current || isQueryingNotification === false) return null;
+    console.log(localAccessToken);
+    if (localAccessToken && localRefreshToken) {
+      if (!authentication.accessToken || !authentication.refreshToken) {
+        setUserAccessToken(localAccessToken);
+        setUserRefreshToken(localRefreshToken);
+      }
+    } else {
+      setUserAccessToken(null);
+      setUserRefreshToken(null);
+      localStorage.removeItem("token");
+      localStorage.removeItem("refreshToken");
+    }
+  }, [localAccessToken, localRefreshToken]);
 
-		if (authentication.accessToken) {
-			setRole(jwtDecode(authentication.accessToken).role);
-			axios
-				.get("/api/users/me")
-				.then((result) => {
-					if (result.status === 200) {
-						setIsAuthenticated(true);
-						getAllInformation(result.data);
-						getAllReceivers(authentication.accessToken);
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-					setIsAuthenticated(false);
-					setUserAccessToken(null);
-					setUserRefreshToken(null);
-					localStorage.removeItem("token");
-					localStorage.removeItem("refreshToken");
-				});
-			getNotificationHistory();
-		}
-		if (!authentication.accessToken && !localAccessToken) {
-			setIsAuthenticated(false);
-			setUserAccessToken(null);
-			setUserRefreshToken(null);
-			localStorage.removeItem("token");
-			localStorage.removeItem("refreshToken");
-		}
-	}, [authentication.accessToken]);
+  // Check if accessToken is valid
+  useEffect(() => {
+    if (!mountedRef.current || isQueryingNotification === false) return null;
 
-	return (
-		<div className="app">
-			<Router>
-				<Switch>
-					{/* https://stackoverflow.com/questions/43520498/react-router-private-routes-redirect-not-working => Newm */}
-					{/* --- LOGIN/PUBLIC COMPONENTS RENDER --- */}
-					{public_routes.map((item, index) => {
-						return (
-							<Route
-								key={index}
-								path={item.path}
-								exact={item.exact ? item.exact : null}
-								component={(props) => (
-									<item.component
-										{...props}
-										{...item.props}
-										isAuthenticated={isAuthenticated}
-									/>
-								)}
-							/>
-						);
-					})}
-					{!isAuthenticated &&
-						public_routes.map((item, index) => {
-							return (
-								<Redirect
-									key={index}
-									to={{
-										pathname: "/login",
-										// state: { from: props.location },
-									}}
-								/>
-							);
-						})}
+    if (authentication.accessToken) {
+      setRole(jwtDecode(authentication.accessToken).role);
+      axios
+        .get("/api/users/me")
+        .then((result) => {
+          if (result.status === 200) {
+            setIsAuthenticated(true);
+            getAllInformation(result.data);
+            getAllReceivers(authentication.accessToken);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsAuthenticated(false);
+          setUserAccessToken(null);
+          setUserRefreshToken(null);
+          localStorage.removeItem("token");
+          localStorage.removeItem("refreshToken");
+        });
+      getNotificationHistory();
+    }
+    if (!authentication.accessToken && !localAccessToken) {
+      setIsAuthenticated(false);
+      setUserAccessToken(null);
+      setUserRefreshToken(null);
+      localStorage.removeItem("token");
+      localStorage.removeItem("refreshToken");
+    }
+  }, [authentication.accessToken]);
 
-					{/* ---CUSTOMER RENDER --- */}
-					{isAuthenticated &&
-						role === "customer" &&
-						customer_routes.map((item, index) => {
-							return (
-								<item.routetype
-									key={index}
-									path={item.path}
-									exact={item.exact ? item.exact : null}
-									isAuthenticated={isAuthenticated}
-									comp={(props) => {
-										return item.layout ? (
-											<item.layout>
-												<item.component {...props} {...item.props} />
-											</item.layout>
-										) : (
-											<item.component {...props} {...item.props} />
-										);
-									}}
-								/>
-							);
-						})}
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          {/* https://stackoverflow.com/questions/43520498/react-router-private-routes-redirect-not-working => Newm */}
+          {/* --- LOGIN/PUBLIC COMPONENTS RENDER --- */}
+          {public_routes.map((item, index) => {
+            return (
+              <Route
+                key={index}
+                path={item.path}
+                exact={item.exact ? item.exact : null}
+                component={(props) => (
+                  <item.component
+                    {...props}
+                    {...item.props}
+                    isAuthenticated={isAuthenticated}
+                  />
+                )}
+              />
+            );
+          })}
+          {!isAuthenticated &&
+            public_routes.map((item, index) => {
+              return (
+                <Redirect
+                  key={index}
+                  to={{
+                    pathname: "/login",
+                    // state: { from: props.location },
+                  }}
+                />
+              );
+            })}
 
-					{/* ---ADMIN RENDER --- */}
-					{isAuthenticated &&
-						role === "admin" &&
-						admin_routes.map((item, index) => {
-							return (
-								<item.routetype
-									key={index}
-									path={item.path}
-									exact={item.exact ? item.exact : null}
-									isAuthenticated={isAuthenticated}
-									comp={(props) => {
-										return item.layout ? (
-											<item.layout>
-												<item.component {...props} {...item.props} />
-											</item.layout>
-										) : (
-											<item.component {...props} {...item.props} />
-										);
-									}}
-								/>
-							);
-						})}
+          {/* ---CUSTOMER RENDER --- */}
+          {isAuthenticated &&
+            role === "customer" &&
+            customer_routes.map((item, index) => {
+              return (
+                <item.routetype
+                  key={index}
+                  path={item.path}
+                  exact={item.exact ? item.exact : null}
+                  isAuthenticated={isAuthenticated}
+                  comp={(props) => {
+                    return item.layout ? (
+                      <item.layout>
+                        <item.component {...props} {...item.props} />
+                      </item.layout>
+                    ) : (
+                      <item.component {...props} {...item.props} />
+                    );
+                  }}
+                />
+              );
+            })}
 
-					{/* ---EMPLOYEE RENDER --- */}
-					{isAuthenticated &&
-						role === "employee" &&
-						employee_routes.map((item, index) => {
-							return (
-								<item.routetype
-									key={index}
-									path={item.path}
-									exact={item.exact ? item.exact : null}
-									isAuthenticated={isAuthenticated}
-									comp={(props) => {
-										return item.layout ? (
-											<item.layout>
-												<item.component {...props} {...item.props} />
-											</item.layout>
-										) : (
-											<item.component {...props} {...item.props} />
-										);
-									}}
-								/>
-							);
-						})}
-				</Switch>
-			</Router>
-		</div>
-	);
+          {/* ---ADMIN RENDER --- */}
+          {isAuthenticated &&
+            role === "admin" &&
+            admin_routes.map((item, index) => {
+              return (
+                <item.routetype
+                  key={index}
+                  path={item.path}
+                  exact={item.exact ? item.exact : null}
+                  isAuthenticated={isAuthenticated}
+                  comp={(props) => {
+                    return item.layout ? (
+                      <item.layout>
+                        <item.component {...props} {...item.props} />
+                      </item.layout>
+                    ) : (
+                      <item.component {...props} {...item.props} />
+                    );
+                  }}
+                />
+              );
+            })}
+
+          {/* ---EMPLOYEE RENDER --- */}
+          {isAuthenticated &&
+            role === "employee" &&
+            employee_routes.map((item, index) => {
+              return (
+                <item.routetype
+                  key={index}
+                  path={item.path}
+                  exact={item.exact ? item.exact : null}
+                  isAuthenticated={isAuthenticated}
+                  comp={(props) => {
+                    return item.layout ? (
+                      <item.layout>
+                        <item.component {...props} {...item.props} />
+                      </item.layout>
+                    ) : (
+                      <item.component {...props} {...item.props} />
+                    );
+                  }}
+                />
+              );
+            })}
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
