31ce19c6593f70905731c9b3ae69b497944d3ac0
thuanphat611 <phat06112003@gmail.com>
2025-01-05 15:29:09 +0700

    Fix duplicate notification in App, sort then show noti length

--- src/BankingUI/Components/Customer/Notification/Notification.js
+++ src/BankingUI/Components/Customer/Notification/Notification.js
@@ -1,13 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import {
-	Container,
-	Col,
-	Card,
-	Row,
-	DropdownButton,
-	Dropdown,
-	ButtonGroup,
-} from "react-bootstrap";
+import { Container, Col, Card, Row, Badge } from "react-bootstrap";
 // import { Button, notification } from "antd";
 import axios from "axios";
 import moment from "moment";
@@ -18,34 +10,39 @@ import openNotification from "../../HelperFunctions/openNotification";
 import NotificationList from "./NotificationList/NotificationList";
 
 const Notification = (props) => {
-	const { reducerUserNotification } = props;
-	const mountedRef = useRef(true);
-	const notificationsDataaaa = reducerUserNotification.data;
+  const { reducerUserNotification } = props;
+  const mountedRef = useRef(true);
+  const notificationsData = reducerUserNotification.data;
 
-	useEffect(() => {
-		if (!mountedRef.current) return null;
+  useEffect(() => {
+    if (!mountedRef.current) return null;
 
-		console.log("COMPONENT DID MOUNT");
+    console.log("COMPONENT DID MOUNT");
 
-		return () => {
-			mountedRef.current = false;
-		};
-	}, []);
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
 
-	return (
-		<Container fluid>
-			<Row>
-				<Col md={{ span: 5, offset: 3 }} lg={6}>
-					<Card className="mt-3">
-						<Card.Header className="toolBar">CÁC THÔNG BÁO</Card.Header>
-						<Card.Body>
-							<NotificationList notificationsData={notificationsDataaaa} />
-						</Card.Body>
-					</Card>
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={{ span: 5, offset: 3 }} lg={6}>
+          <Card className="mt-3">
+            <Card.Header className="toolBar">
+              <span>
+                CÁC THÔNG BÁO{" "}
+                <Badge variant="success">{notificationsData.length}</Badge>
+              </span>
+            </Card.Header>
+            <Card.Body>
+              <NotificationList notificationsData={notificationsData} />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Notification;
