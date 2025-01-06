acc246c864eb5f08b23fcb7e27ce1fbe92320de1
thuanphat611 <phat06112003@gmail.com>
2025-01-06 11:56:59 +0700

    Change steps' name

--- src/BankingUI/Components/Customer/Transaction/OtpForm/OtpForm.js
+++ src/BankingUI/Components/Customer/Transaction/OtpForm/OtpForm.js
@@ -1,104 +1,142 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button, Form, Col, Row } from "react-bootstrap";
+import { Button, Form, Row, Col, Card, Spinner } from "react-bootstrap";
+import { Link, Redirect } from "react-router-dom";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackward } from "@fortawesome/free-solid-svg-icons";
+
 import axios from "axios";
+import { axiosInstance } from "../../../../App/App";
 
-const OtpForm = (props) => {
-	const {
-		formVariables,
-		setFormVariables,
-		accessToken,
-		setStep,
-		balance,
-		setFormError,
-	} = props;
-	const [validated, setValidated] = useState(false);
+const OtpForm = ({
+  formVariables,
+  setFormVariables,
+  step,
+  setStep,
+  setFormError,
+  history,
+}) => {
+  const [validated, setValidated] = useState(false);
 
-	// Update giá trị điền vào Form
-	const handleChange = (e) => {
-		formVariables[e.target.name] = e.target.value;
-		setFormVariables({ ...formVariables });
-	};
+  const handleSubmit = async (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (
+      form.checkValidity() === false ||
+      formVariables.retypeNewPassword !== formVariables.newPassword ||
+      formVariables.email === ""
+    ) {
+      event.stopPropagation();
+    } else {
+      await axiosInstance
+        .post(`/api/auth/verify-forgot-password`, {
+          code: formVariables.otp,
+          email: formVariables.email,
+          newPassword: formVariables.newPassword,
+        })
+        .then((result) => {
+          if (result.status === 200) {
+            setFormError(
+              null,
+              (result.data && result.data.message) ||
+                "Changed password successfully!"
+            );
+            setTimeout(() => {
+              setStep("redirect");
+            }, 5000);
+          }
+        })
+        .catch((err) => {
+          console.log("login: ", err);
+          setFormError(
+            true,
+            (err.response && err.response.data && err.response.data.message) ||
+              "Something's wrong"
+          );
+        });
+      // if (false) {
+      // 	setStep(2);
+      // } else {
+      // 	setFormError("This OTP Code is invalid. Please try again!");
+      // }
+    }
+    setValidated(true);
+  };
 
-	const handleSubmit = async (event) => {
-		const form = event.currentTarget;
-		event.preventDefault();
-		if (
-			form.checkValidity() === false ||
-			formVariables.name == "" ||
-			formVariables.name == "KHONG TIM THAY"
-		) {
-			event.stopPropagation();
-		} else {
-			await axios
-				.post(
-					"/api/transaction/verify-code",
-					{
-						code: formVariables.otp,
-					},
-					{
-						headers: {
-							transactionId: formVariables.transactionId,
-						},
-					}
-				)
-				.then((result) => {
-					console.log(result);
-					setFormVariables({ ...formVariables, isLoading: false });
-					if (result.status === 200) {
-						setStep(4);
-						setFormError(null, "");
-					}
-				})
-				.catch((err) => {
-					console.log(err.response);
-					err.response.message
-						? setFormError(true, err.response.data.message)
-						: setFormError(true, "Something's wrong, please try again!");
-				});
-		}
-		setValidated(true);
-	};
+  const handleChange = (e) => {
+    formVariables[e.target.name] = e.target.value;
+    setFormVariables({ ...formVariables });
+  };
 
-	return (
-		<>
-			<h5>Step 3: Send your OTP received from mail</h5>
-			<hr />
-			<Form noValidate validated={validated} onSubmit={handleSubmit}>
-				<Form.Group>
-					<p>
-						You're about to send {formVariables.amount} to user{" "}
-						<span>{formVariables.name}</span>
-					</p>
-					<Form.Text className="text-muted font-weight-bold">
-						OTP Code
-					</Form.Text>
-					<Form.Control
-						required
-						type="text"
-						name="otp"
-						value={formVariables.otp}
-						onChange={(e) => handleChange(e)}
-						isInvalid={formVariables.otp === ""}
-					/>
-					<Form.Control.Feedback type="invalid">
-						Give your receiver a message to know
-					</Form.Control.Feedback>
-				</Form.Group>
-				<Button variant="primary" type="submit">
-					Next
-				</Button>
-				<Button
-					variant="primary-outline"
-					type="button"
-					onClick={() => setStep(2)}
-				>
-					<FontAwesomeIcon icon={faBackward} /> Back
-				</Button>
-			</Form>
-		</>
-	);
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicUsername">
+        <Form.Label className="font-weight-bold">
+          Your received OTP code
+        </Form.Label>
+        <Form.Control
+          required
+          type="text"
+          name="otp"
+          placeholder=""
+          value={formVariables.otp}
+          onChange={(e) => handleChange(e)}
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Please provide us your received OTP code.
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className="font-weight-bold">Your new password</Form.Label>
+        <Form.Control
+          required
+          type="password"
+          name="newPassword"
+          placeholder=""
+          value={formVariables.newPassword}
+          onChange={(e) => handleChange(e)}
+        />
+        <Form.Text className="text-muted">This is your new password.</Form.Text>
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Retype password doesn't match.
+        </Form.Control.Feedback>
+        <Form.Label className="font-weight-bold">
+          Retype your new password
+        </Form.Label>
+        <Form.Control
+          required
+          type="password"
+          name="retypeNewPassword"
+          placeholder=""
+          value={formVariables.retypeNewPassword}
+          onChange={(e) => handleChange(e)}
+          isInvalid={
+            formVariables.retypeNewPassword !== formVariables.newPassword
+          }
+        />
+        <Form.Control.Feedback type="invalid">
+          Retype password doesn't match.
+        </Form.Control.Feedback>
+
+        <Form.Text className="mt-2">
+          <Link to="/login">Go back to Login screen!</Link>
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" className="mt-3">
+        Confirm
+      </Button>
+      <Button
+        variant="primary-outline"
+        type="button"
+        className="mt-3"
+        onClick={() => setStep("email-form")}
+      >
+        <FontAwesomeIcon icon={faBackward} /> Add email
+      </Button>
+    </Form>
+  );
 };
 
 export default OtpForm;
